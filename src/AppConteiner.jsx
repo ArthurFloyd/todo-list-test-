7169a0fbcf1eca377a58a5f941aa1be77395bd96
arthur voryshev <what_to_write@mail.ru>
2024-08-04 14:01:42 +0300

    minor fixes

--- src/AppConteiner.jsx
+++ src/AppConteiner.jsx
@@ -1,5 +1,7 @@
 function AppConteiner() {
-  return <div className="d-flex flex-column h-100"></div>;
+  return <div className="d-flex flex-column h-100">
+    <div></div>
+  </div>;
 }
 
 export default AppConteiner
